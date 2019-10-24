dcd87a03a74412f17fec0b39f56ff4d233fbee03
Andreas Linnert <andreas@linnertmedia.de>
2019-10-24 22:26:17 +0200

    Use more compact syntax in main function

--- src/main.dart
+++ src/main.dart
@@ -5,12 +5,11 @@ import 'package:args/command_runner.dart';
 import 'commands/config/configCommand.dart';
 
 void main(List<String> arguments) {
-  final runner = new CommandRunner("sshtool", "A ssh management tool")
-    ..addCommand(new ConfigCommand());
-
-  runner.run(arguments).catchError((error) {
-    if (error is! UsageException) throw error;
-    print(error);
-    exit(64);
-  });
+  new CommandRunner("sshtool", "A ssh management tool")
+    ..addCommand(new ConfigCommand())
+    ..run(arguments).catchError((error) {
+      if (error is! UsageException) throw error;
+      print(error);
+      exit(64);
+    });
 }
