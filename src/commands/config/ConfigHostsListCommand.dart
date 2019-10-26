76e12259c6799b0ada0d117b92c5fe0ede81a23d
Andreas Linnert <andreas@linnertmedia.de>
2019-10-26 22:30:44 +0200

    Update ConfigService structure
    Move from class with static fields to simple functions and variables.

--- src/commands/config/ConfigHostsListCommand.dart
+++ src/commands/config/ConfigHostsListCommand.dart
@@ -1,13 +1,13 @@
 import 'package:args/command_runner.dart';
 
-import '../../services/ConfigService.dart';
+import '../../services/ConfigService.dart' show getHostNames;
 
 class ConfigHostsListCommand extends Command<void> {
   final name = 'list';
   final description = 'List all available hosts';
 
   void run() {
-    final hostnames = ConfigService.getHostNames();
+    final hostnames = getHostNames();
     print(hostnames.join('\n'));
   }
 }
--- src/services/ConfigService.dart
+++ src/services/ConfigService.dart
@@ -1,56 +1,51 @@
 import 'dart:io';
 
-class ConfigService {
-  static List<String> fileContent = null;
+List<String> fileContent = null;
 
-  static List<String> _getUserConfigFileContent(
-      {bool global = false, bool force = false}) {
-    final filename = _getConfigFilePath(global);
+List<String> _getUserConfigFileContent(
+    {bool global = false, bool force = false}) {
+  final filename = _getConfigFilePath(global);
+  if (fileContent == null || force) {
+    final file = new File(filename);
+    fileContent = file.readAsLinesSync();
+  }
+  return fileContent;
+}
 
-    if (fileContent == null || force) {
-      final file = new File(filename);
-      fileContent = file.readAsLinesSync();
+String _getConfigFilePath(bool global) {
+  final env = Platform.environment;
+  final homeDir = Platform.isWindows ? env['UserProfile'] : env['HOME'];
+
+  if (global) {
+    if (Platform.isWindows) {
+      // This gets tricky...
+      // The config file is bundled with OpenSSH.
+      // Its location can by anywhere:
+      // - Whereever the user installs Git
+      // - The OpenSSH that comes with Windows'
+      // Plus: multiple versions could co-exist.
+      // Have fun, future me. Greetings from the past. _o/
+      // But maybe this also affects other OSes.
+      throw Exception('not yet implemented');
+    } else {
+      return '/etc/ssh/ssh_config';
     }
-
-    return fileContent;
   }
 
-  static String _getConfigFilePath(bool global) {
-    final env = Platform.environment;
-    final homeDir = Platform.isWindows ? env['UserProfile'] : env['HOME'];
+  return homeDir + '/.ssh/config';
+}
 
-    if (global) {
-      if (Platform.isWindows) {
-        // This gets tricky...
-        // The config file is bundled with OpenSSH.
-        // Its location can by anywhere:
-        // - Whereever the user installs Git
-        // - The OpenSSH that comes with Windows'
-        // Plus: multiple versions could co-exist.
-        // Have fun, future me. Greetings from the past. _o/
-        // But maybe this also affects other OSes.
-        throw Exception('not yet implemented');
-      } else {
-        return '/etc/ssh/ssh_config';
-      }
-    }
+List<String> _getHostsFromConfigFileContent(List<String> lines) {
+  final hostLines = lines
+      .where((line) => line.startsWith('Host '))
+      .map((line) => line.replaceFirst('Host ', ''))
+      .toList();
 
-    return homeDir + '/.ssh/config';
-  }
+  return hostLines;
+}
 
-  static List<String> _getHostsFromConfigFileContent(List<String> lines) {
-    final hostLines = lines.where((line) {
-      return line.startsWith('Host ');
-    }).map((line) {
-      return line.replaceFirst('Host ', '');
-    }).toList();
-
-    return hostLines;
-  }
-
-  static List<String> getHostNames({bool global = false}) {
-    final lines = _getUserConfigFileContent(global: global, force: false);
-    final hosts = _getHostsFromConfigFileContent(lines);
-    return hosts;
-  }
+List<String> getHostNames({bool global = false}) {
+  final lines = _getUserConfigFileContent(global: global, force: false);
+  final hosts = _getHostsFromConfigFileContent(lines);
+  return hosts;
 }
